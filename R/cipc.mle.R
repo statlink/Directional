d783183fed4b558ec02cfc1c546eab6e397483cb
Michail Tsagris <mtsagris@yahoo.gr>
2024-04-23 19:09:50 +0300

    change

--- R/cipc.mle.R
+++ R/cipc.mle.R
@@ -8,42 +8,46 @@ cipc.mle <- function(x, rads = FALSE, tol = 1e-6) {
   g2 <- sum(mu^2)
   a <- as.vector(x %*% mu)
   com <- sqrt(g2 + 1)
-  com2 <- com - a
+  com2 <- 1 / (com - a)
   lik <- -sum( log( sqrt(g2 + 1) - a ) )
-  up <- Rfast::eachrow(xm, mu / com, oper = "+" )
-  der <-  - Rfast::colsums(up / com2 )
-  up1 <- outer( as.vector( ( diag(com, 2) - tcrossprod(mu) / com ) / com^2 ), com2, FUN = "/" )
-  up1 <- matrix( Rfast::rowsums(up1), ncol = 2)
-  up2 <- crossprod(up, up / com2^2)
-  der2 <-  -(up1 - up2 )
+  up <- Rfast::eachrow(x, mu / com, oper = "-" )
+  der <-  Rfast::colsums(up * com2 )
+  #up1 <- Rfast::Outer( as.vector( ( diag(com, 2) - tcrossprod(mu) / com ) / com^2 ), com2, oper = "/" )
+  #up1 <- matrix( Rfast::colsums(up1), ncol = 2)
+  up1 <- ( diag(com, 2) - tcrossprod(mu) / com ) / com^2 * sum(com2)
+  up2 <- crossprod(up * com2)
+  der2 <- up2 - up1
 
   mu <- mu - solve(der2, der)
   g2 <- sum(mu^2)
   a <- as.vector(x %*% mu)
   com <- sqrt(g2 + 1)
-  com2 <- com - a
-  lik[2] <-  - sum( log( sqrt(g2 + 1) - a ) )
+  com2 <- 1 / (com - a)
+  lik[2] <-  sum( log( com2 ) )
 
   i <- 2
   while ( lik[i] - lik[i - 1] > tol ) {
     i <- i + 1
-    up <- Rfast::eachrow(xm, mu / com, oper = "+" )
-    der <-  - Rfast::colsums(up / com2 )
-    up1 <- outer( as.vector( ( diag(com, 2) - tcrossprod(mu) / com ) / com^2 ), com2, FUN = "/" )
-    up1 <- matrix( Rfast::rowsums(up1), ncol = 2)
-    up2 <- crossprod(up, up / com2^2)
-    der2 <-  -(up1 - up2 )
+    up <- Rfast::eachrow(x, mu / com, oper = "-" )
+    der <-  Rfast::colsums(up * com2 )
+    #up1 <- Rfast::Outer( as.vector( ( diag(com, 2) - tcrossprod(mu) / com ) / com^2 ), com2, oper = "/" )
+    #up1 <- matrix( Rfast::colsums(up1), ncol = 2)  
+    up1 <- ( diag(com, 2) - tcrossprod(mu) / com ) / com^2 * sum(com2)
+    up2 <- crossprod(up * com2)
+    der2 <- up2 - up1
     mu <- mu - solve(der2, der)
     a <- as.vector(x %*% mu)
     g2 <- sum(mu^2)
     com <- sqrt(g2 + 1)
-    com2 <- com - a
-    lik[i] <- -sum( log( sqrt(g2 + 1) - a ) )
+    com2 <- 1 / ( com - a )
+    lik[i] <-  sum( log( com2 ) )
   }
   circmu <- ( atan(mu[2]/mu[1]) + pi * I(mu[1] < 0) ) %% (2 * pi)
   gama <- sqrt( sum(mu^2) )
   list(mu = mu, circmu = circmu, gamma = sqrt(g2), loglik = lik[i] - n * log(2 * pi) )
 }
+
+
 
 
 
