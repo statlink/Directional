abb65fe03ad3580553ba085bdb5726ca6f9f1fd6
Michail Tsagris <mtsagris@yahoo.gr>
2024-09-01 08:04:42 +0300

    change

--- R/iagd.R
+++ R/iagd.R
@@ -1,13 +1,16 @@
-iagd <- function(y, mu, logden = FALSE) {
+IAGd <- function(y, mu, logden = FALSE) {  
   y <- as.matrix(y)
-  p <- dim(y)[2]
-  if ( p == 1 )   y <- t(y)
-  a <- tcrossprod(mu, y)
-  a2 <- a^2
-  pa <- pnorm(a)
-  da <- dnorm(a)
-  gm <- pa + a2 * pa + a * da
-  den <-  -log(2 * pi) + 0.5 * a2 - 0.5 * sum(mu^2) + log(gm)
-  if ( !logden )  den <- exp(den)
-  den
+  if  ( dim(y)[2] == 1 )   y <- t(y)
+  d <- dim(y)[2]
+  p <- d - 1
+  Cd <- (2 * pi)^(-0.5 * p)
+  a <- as.vector( y %*% mu )
+  Mp <- matrix(1, nrow = n, ncol = p)
+  Mp[, 1] <- a * pnorm(a) + dnorm(a)
+  Mp[, 2] <- (1 + a^2) * pnorm(a) + a * dnorm(a)
+  if ( p >= 3 )   for ( j in 3:p )  Mp[, j] <- a * Mp[, j - 1] + (j - 1) * Mp[, j - 2]
+  l <- log(Cd) + 0.5*( a^2 - sum(mu^2) ) + log( Mp[, p] )
+  if ( logden )  {
+    return( l )
+  } else   return( exp(l) )
 }
