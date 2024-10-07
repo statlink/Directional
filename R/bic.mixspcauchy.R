eebc52a8de80e002e8f9b48705f0475a9fd789e1
Michail Tsagris <mtsagris@yahoo.gr>
2024-10-07 18:43:39 +0300

    change

--- R/bic.mixspcauchy.R
+++ R/bic.mixspcauchy.R
@@ -1,13 +1,4 @@
-################################
-#### BIC to choose the number of components in a
-#### model based clustering using mixtures of von Mises-Fisher distributions
-#### Tsagris Michail 4/2015
-#### mtsagris@yahoo.gr
-#### References: Kurt Hornik and  Bettina Grun (2014)
-#### movMF: An R Package for Fitting Mixtures of von Mises-Fisher Distributions
-#### http://cran.r-project.org/web/packages/movMF/vignettes/movMF.pdf
-################################
-bic.mixvmf <- function(x, G = 5, n.start = 10) {
+bic.mixspcauchy <- function(x, G = 5, n.start = 10) {
   ## x contains the data
   ## A is the maximum number of clusters, set to 3 by default
   runtime <- proc.time()
@@ -17,7 +8,7 @@ bic.mixvmf <- function(x, G = 5, n.start = 10) {
   mod <- Directional::vmf.mle(x)
   bic[1] <-  - 2 * mod$loglik+ p * logn  ## BIC assuming one cluster
   for (vim in 2:G) {
-    a <- Directional::mixvmf.mle(x, vim, n.start = n.start)  ## model based clustering for some possible clusters
+    a <- Directional::mixspcauchy.mle(x, vim, n.start = n.start)  ## model based clustering for some possible clusters
     bic[vim] <-  -2 * a$loglik + ( (vim - 1) + vim * p ) * logn
   }  ## BIC for a range of different clusters
   runtime <- proc.time() - runtime
@@ -26,7 +17,7 @@ bic.mixvmf <- function(x, G = 5, n.start = 10) {
   ina[which.min(bic)] <- 3  ## chosen number of clusters will
   ## appear with red on the plot
   plot(1:G, bic, col = ina, xlab = "Number of components",
-  ylab = "BIC values")
+       ylab = "BIC values")
   abline(v = 1:G, lty = 2, col = "lightgrey")
   abline(h = seq(min(bic), max(bic), length = 10), lty = 2, col = "lightgrey" )
   lines(1:G, bic, lwd = 2)
