9b6df15fa4e1c771addc4379e0f25e2a13f04ea3
Michail Tsagris <mtsagris@yahoo.gr>
2024-10-23 11:24:22 +0300

    change

--- R/icl.mixspcauchy.R
+++ R/icl.mixspcauchy.R
@@ -1,4 +1,4 @@
-bic.mixspcauchy <- function(x, G = 5, n.start = 5, tol = 1e-6, maxiters = 500) {
+icl.mixspcauchy <- function(x, G = 5, n.start = 5, tol = 1e-6, maxiters = 500) {
   ## x contains the data
   ## A is the maximum number of clusters, set to 3 by default
   runtime <- proc.time()
@@ -9,13 +9,13 @@ bic.mixspcauchy <- function(x, G = 5, n.start = 5, tol = 1e-6, maxiters = 500) {
   bic[1] <-  - 2 * mod$loglik+ p * logn  ## BIC assuming one cluster
   for (vim in 2:G) {
     a <- Directional::mixspcauchy.mle(x, vim, n.start = n.start, tol = tol, maxiters = maxiters)  ## model based clustering for some possible clusters
-    bic[vim] <-  -2 * a$loglik + ( (vim - 1) + vim * p ) * logn
+    bic[vim] <-  -2 * a$loglik + ( (vim - 1) + vim * p ) * logn + sum( a$w * log(a$w), na.rm = TRUE )
   }  ## BIC for a range of different clusters
   runtime <- proc.time() - runtime
   names(bic) <- 1:G
   ina <- rep(1, G)
   ina[ which.min(bic) ] <- 3  ## chosen number of clusters will appear with red on the plot
-  plot(1:G, bic, col = ina, xlab = "Number of components", ylab = "BIC values", cex.lab = 1.3, cex.axis = 1.3)
+  plot(1:G, bic, col = ina, xlab = "Number of components", ylab = "ICL values", cex.lab = 1.3, cex.axis = 1.3)
   abline(v = 1:G, lty = 2, col = "lightgrey")
   abline(h = seq(min(bic, na.rm = FALSE), max(bic, na.rm = FALSE), length = 10), lty = 2, col = "lightgrey" )
   lines(1:G, bic, lwd = 2)
